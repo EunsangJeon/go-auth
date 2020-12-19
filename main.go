93bf19a265ebbdac35a94ee151e7187d2639dfab
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-12-19 17:53:06 +0900

    bcrypt example

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type person struct {
@@ -10,11 +12,33 @@ type person struct {
 }
 
 func main() {
-	// jsonExample()
+	pass := "123456789"
 
-	// http.HandleFunc("/encode", foo)
-	// http.HandleFunc("/decode", bar)
-	// http.ListenAndServe(":8080", nil)
+	hashedPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+	err = comparePassword(pass, hashedPass)
+	if err != nil {
+		log.Fatalln("Not logged in")
+	}
+
+	log.Println("Logged in!")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error while generating hashed password: %w", err)
+	}
+	return bs, nil
+}
+
+func comparePassword(password string, hashedPass []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+	if err != nil {
+		return fmt.Errorf("Invalid password: %w", err)
+	}
+	return nil
 }
