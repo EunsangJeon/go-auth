6adc3a9cce3e1f6ebc6f15d23ceccd920d14050c
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-12-19 15:57:35 +0900

    encode

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -17,7 +19,14 @@ func main() {
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
+	p1 := person{
+		First: "John",
+	}
 
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("Encoded bad data:", err)
+	}
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
