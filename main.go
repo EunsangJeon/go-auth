83eae48bf5560769130c7a14ecc8d4ba5180dffb
Eunsang <nicolaus1207@gmail.com>
2020-12-19 15:05:07 +0900

    unmarshal

--- main.go
+++ main.go
@@ -26,5 +26,14 @@ func main() {
 		log.Panic(err)
 	}
 
-	fmt.Println(string(bs))
+	fmt.Println("Print JSON", string(bs))
+
+	xp2 := []person{}
+
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Back to Go data struct", xp2)
 }
