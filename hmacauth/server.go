f7a5ab41a552ba6ef9039427ee9b16fa1398ddfb
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-12-23 07:59:06 +0900

    setting cookie with hmac

--- hmacauth/server.go
+++ hmacauth/server.go
@@ -1,6 +1,8 @@
 package hmacauth
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
@@ -17,7 +19,11 @@ func Server() {
 	}
 }
 
-func getCode()
+func getCode(msg string) string {
+	h := hmac.New(sha256.New, []byte("keyishere"))
+	h.Write([]byte(msg))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
 
 func bar(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -30,14 +36,23 @@ func bar(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 
+	code := getCode(email)
+
 	c := http.Cookie{
 		Name:  "session",
-		Value: "",
+		Value: code + "|" + email,
 	}
-	fmt.Println(c)
+
+	http.SetCookie(w, &c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session")
+	if err != nil {
+		c = &http.Cookie{}
+	}
+
 	html := `
 		<!DOCTYPE html>
 		<html lang="en">
@@ -48,6 +63,7 @@ func foo(w http.ResponseWriter, r *http.Request) {
 			<title>HMAC Example</title>
 		</head>
 		<body>
+			<p>Cookie value: ` + c.Value + `</p>
 			<form action="/submit" method="post">
 				<input type="email" name="email" />
 				<input type="submit" />
